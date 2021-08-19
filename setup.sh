b5cb8b45d0cef214ed487be103f2775996d07135
Michał <43048524+mich4ld@users.noreply.github.com>
2021-08-19 19:09:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,11 @@ git clone https://github.com/agkozak/zsh-z  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/p
 
 echo "===> Installing powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "===> Changing zsh theme to powerlevel10k..."
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
+
+echo "===> Enabling zsh plugins..."
+sed -i 's/plugins=(git)/plugins=(git history-substring-search colored-man-pages zsh-autosuggestions zsh-syntax-highlighting zsh-z)/g' ~/.zshrc
+
+echo "Zsh plugins and theme installed"
