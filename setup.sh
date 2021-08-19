15120dbf4753d5e4d9094d29a2af772c44380826
Michał <43048524+mich4ld@users.noreply.github.com>
2021-08-19 15:04:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-echo "Installing oh my zsh..."
+echo "===> Installing oh my zsh..."
 RUNZSH=no sh -c "$(curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Installing some zsh plugins..."
+echo "===> Installing some zsh plugins..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/agkozak/zsh-z  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+
+echo "===> Installing powerlevel10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
